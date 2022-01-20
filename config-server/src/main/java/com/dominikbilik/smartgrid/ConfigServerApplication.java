121d169717d4a48c822b86ad5607285c5622acc8
Dominik Bilik <dominikbilik4@gmail.com>
2022-01-20 22:17:30 +0100

    initial

---
+++ config-server/src/main/java/com/dominikbilik/smartgrid/ConfigServerApplication.java
@@ -0,0 +1,4 @@
+package com.dominikbilik.smartgrid;
+
+public class ConfigServerApplication {
+}
---
+++ measured-data-service/src/main/java/com.dominikbilik.smartgrid.measureddata/MeasuredDataApplication.java
@@ -0,0 +1,13 @@
+package com.dominikbilik.smartgrid.measureddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasuredDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeasuredDataApplication.class, args);
+    }
+
+}
